69a0723798088ebc7bdb271022f52643c9b07624
Alex Spataru <alex.racotta@gmail.com>
2014-11-05 11:52:35 -0600

    Bug fixes in the QSimpleUpdater

--- src/libs/QSimpleUpdater/src/qsimpleupdater.h
+++ src/libs/QSimpleUpdater/src/qsimpleupdater.h
@@ -13,49 +13,48 @@
 #include <QSslConfiguration>
 #include <QNetworkAccessManager>
 
-class QSimpleUpdater : public QObject
-{
-        Q_OBJECT
+class QSimpleUpdater : public QObject {
+    Q_OBJECT
 
-    public:
-        QSimpleUpdater (QObject *parent = 0);
+public:
+    QSimpleUpdater(QObject *parent = 0);
 
-        QString changeLog();
-        void checkForUpdates();
-        QString latestVersion();
-        QString installedVersion();
-        void downloadLatestVersion();
-        bool newerVersionAvailable();
+    QString changeLog();
+    void checkForUpdates();
+    QString latestVersion();
+    QString installedVersion();
+    void downloadLatestVersion();
+    bool newerVersionAvailable();
 
-    public slots:
-        void setDownloadUrl (const QString url);
-        void setReferenceUrl (const QString url);
-        void setChangelogUrl (const QString url);
-        void setApplicationVersion (const QString version);
+public slots:
+    void setDownloadUrl(const QString url);
+    void setReferenceUrl(const QString url);
+    void setChangelogUrl(const QString url);
+    void setApplicationVersion(const QString version);
 
-    private slots:
-        void checkDownloadedVersion (QNetworkReply *reply);
-        void processDownloadedChangelog (QNetworkReply *reply);
-        void ignoreSslErrors (QNetworkReply *reply, QList<QSslError> error);
+private slots:
+    void checkDownloadedVersion(QNetworkReply *reply);
+    void processDownloadedChangelog(QNetworkReply *reply);
+    void ignoreSslErrors(QNetworkReply *reply, QList<QSslError> error);
 
-    signals:
-        void checkingFinished();
-        void versionCheckFinished();
-        void changelogDownloadFinished();
+signals:
+    void checkingFinished();
+    void versionCheckFinished();
+    void changelogDownloadFinished();
 
-    private:
-        QString m_changelog;
-        QString m_latest_version;
-        QString m_installed_version;
+private:    
+    QString m_changelog;
+    QString m_latest_version;
+    QString m_installed_version;
 
-        QUrl m_download_url;
-        QUrl m_reference_url;
-        QUrl m_changelog_url;
+    QUrl m_download_url;
+    QUrl m_reference_url;
+    QUrl m_changelog_url;
 
-        bool m_changelog_downloaded;
-        bool m_version_check_finished;
+    bool m_changelog_downloaded;
+    bool m_version_check_finished;
 
-        bool m_new_version_available;
+    bool m_new_version_available;
 };
 
 #endif
